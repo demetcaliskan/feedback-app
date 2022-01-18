e7190ba96718ec9b3bbd5be1956bb839c3a98a2c
demetcaliskan <demetcaliskann@gmail.com>
2022-01-18 21:15:48 +0300

    FeedbackItem component is created

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,10 @@
+function FeedbackItem() {
+    return (
+        <div className='card'>
+            <div className='num-display'></div>
+            <div className='text-display'></div>
+        </div>
+    )
+}
+
+export default FeedbackItem
