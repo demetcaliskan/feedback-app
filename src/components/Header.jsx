cda178eecca697bb6b39beac259569402d9f9168
demetcaliskan <demetcaliskann@gmail.com>
2022-01-18 21:24:34 +0300

    updated header with some style props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types'
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+
     return (
-        <header>
+        <header style={headerStyles}>
             <div className="container">
                 <h2>{text}</h2>
             </div>
@@ -11,7 +17,9 @@ function Header({ text }) {
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+    bgColor: 'rgba(0, 0, 0, 0.4)',
+    textColor: '#ff6a95'
 }
 
 Header.propTypes = {
