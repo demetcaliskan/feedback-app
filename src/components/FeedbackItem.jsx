731c5678dc57927133f7f3f84f46c08f2d67e594
demetcaliskan <demetcaliskann@gmail.com>
2022-01-18 21:24:16 +0300

    added a useState hook to FeedbackItem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
 function FeedbackItem() {
+    const [rating, setRating] = useState(7)
+    const [text, setText] = useState('This is an example of a feedback item.')
+
+
     return (
         <div className='card'>
-            <div className='num-display'></div>
-            <div className='text-display'></div>
+            <div className='num-display'>{rating}</div>
+            <div className='text-display'>{text}</div>
         </div>
     )
 }
